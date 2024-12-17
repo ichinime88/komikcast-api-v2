a4d29b79d06d1437a020bff7e2294ca0e4746df0
ichinime88 <ichinimeofficial@gmail.com>
2024-12-17 16:41:21 +0700

    Update router.js

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ const router = require("express")();
 const cheerio = require("cheerio");
 const { AxiosService } = require("./helper/axios_service");
 const { responseApi } = require("./helper/response_api");
-const baseUrl = "https://komikcast.cz";
+const baseUrl = "https://komikcast.bz";
 
 router.get("/", (req, res) => {
   return res.status(200).json({
